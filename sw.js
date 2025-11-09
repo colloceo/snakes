9b30d8209af4c05fc69cee785c9a46369e408d9f
Collins Otieno <otienocollins0549@gmail.com>
2025-11-09 15:39:30 +0300

    Add offline support with service worker

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'snakes-ladders-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/snakes.jpg',
+  '/favicon1.jpeg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
